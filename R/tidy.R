903ddeecdc5b3a279233cfd7fdb8ae0a2798796d
benjaminguinaudeau <bguinaudeau@gmail.com>
2019-12-18 16:04:51 -0500

    looooooot of fixes + one function for selenium and xml elements

--- R/tidy.R
+++ R/tidy.R
@@ -1,75 +1,274 @@
-
 #' tidy_element
-#' @param elems a list of selenium-elements 
+#' @param element a list of xml nodes representing individual divs or a list of selenium-elements 
 #' @param depth a numeric value indicating how deep the search should go.
 #' @return A tibble with the elements attribute and a column for each layer containing the children from the corresponding layer.
 #' @export
 
-tidy_element <- function(elems, depth = 0){
+tidy_element <- function(element, depth = 0){
   
-  out <- elems %>%
-    purrr::map_dfr(tidyweb::get_all_attribute) %>%
+  if(!class(element)[1] %in% c("xml_nodeset", "list")) element <- list(element)
+  
+  if(class(element[[1]])[1] == "xml_node"){
+    get_children <- xml2::xml_children
+    get_attr <- get_attributes
+  } else {
+    get_children <- function(x) find_children(x, "*", "xpath")
+    get_attr <- tidyselenium::get_all_attribute
+  }
+  
+  
+  
+  out <- element %>% 
+    purrr::map_dfr(get_attr, text = T) %>%
     dplyr::mutate(id_parent = 1:n())
   
   depth_index <- 0
   
   while(depth_index != depth){
     
-    depth_index <- depth_index + 1
-    message("Current depth: ", depth_index)
-    child_col <- paste0("children_", depth_index)
-    child_id_col <- paste0("id_children_", depth_index)
-    element_col <- paste0("element_", "depth_index")
+    parent_col <- dplyr::sym(paste0("children_", depth_index))
     
-    if(depth_index != 1){
-      parent_col <- dplyr::sym(paste0("children_", depth_index - 1))
-      if(all(out %>% dplyr::pull({{parent_col}}) %>% purrr::map_lgl(~length(.x) == 0))){
+    if(depth_index != 0){
+      if(all(out[[parent_col]] %>% purrr::map_lgl(~length(.x) == 0))){
         message("Max depth was reached")
-        # out <- out %>% select(-parent_col)
-        return(out)
+        break(out)
       }
     }
     
+    depth_index <- depth_index + 1
+    utils::flush.console()
+    cat("\rCurrent depth: ", depth_index)
+    
+    child_col <- paste0("children_", depth_index)
+    child_id_col <- paste0("id_children_", depth_index)
+    
+    
     if(depth_index == 1){
       
-      out <- out %>%
+      out <- out %>% 
         dplyr::mutate({{child_col}} := purrr::map2(element, id_parent,~{
           .x %>%
-            tidyweb::find_children("*", "xpath") %>%
-            purrr::map_dfr(tidyweb::get_all_attribute) %>%
+            get_children %>%
+            purrr::map_dfr(get_attr, text = T)  %>%
             dplyr::mutate(id_parent = .y,
-                   {{child_id_col}} := 1:n(),
-                   depth = depth_index)
+                          {{child_id_col}} := 1:dplyr::n(),
+                          depth = {{depth_index}})
         }))
-    } else {
       
-      out <- out %>%
-        mutate(parents = {{parent_col}}) %>%
-        mutate({{child_col}} := map(parents, ~{
-          if(length(.x) != 0){
-            parent <- .x
-            # child <- children[[1]]
-            
-            parent %>%
-              split(1:nrow(.)) %>%
-              imap_dfr(~{
-                children <- .x$element[[1]] %>%
-                  find_children("*", "xpath")
-                
-                if(length(children) > 0){
-                  children_attr <- children %>%
-                    purrr::map_dfr(tidyweb::get_all_attribute) %>%
-                    cbind(dplyr::select(.x, dplyr::contains("id_"))) %>%
-                    dplyr::as_tibble() %>%
-                    dplyr::mutate({{child_id_col}} := 1:n(),
-                           depth = depth_index)
-                }
-              })
-          }
-        })) %>%
+    } else {
+      out <-  out %>%
+        dplyr::mutate(parents = {{parent_col}}, 
+                      {{child_col}} := purrr::map(parents, ~{
+                        
+                        if(length(.x) != 0){
+                          
+                          .x %>%
+                            split(1:nrow(.)) %>%
+                            purrr::imap_dfr(~{
+                              children <-  .x$element[[1]] %>%
+                                get_children
+                              
+                              if(length(children) > 0){
+                                children_attr <-   children %>%
+                                  purrr::map_dfr(get_attr, text = T) %>%
+                                  dplyr::filter(!is.null(element)) %>%
+                                  cbind(dplyr::select(.x, dplyr::contains("id_"))) %>%
+                                  dplyr::as_tibble() %>%
+                                  dplyr::mutate({{child_id_col}} := 1:dplyr::n(),
+                                                depth = {{depth_index}})
+                                
+                                return(children_attr)
+                              }
+                              
+                            })
+                        }
+                      })) %>%
         dplyr::select(-parents)
     }
+    
+    
   }
+  
+  out <- out %>%
+    dplyr::select(dplyr::contains("children")) %>%
+    purrr::reduce(dplyr::bind_rows) %>%
+    dplyr::bind_rows(dplyr::select(out, -dplyr::contains("children_")), .) %>%
+    dplyr::mutate_at(dplyr::vars(dplyr::contains("id_children"), id_parent), as.character) %>%
+    split(1:nrow(.)) %>%
+    purrr::map_dfr(get_unique_id) %>%
+    dplyr::select(.id, element, text, dplyr::everything())
+  
   return(out)
 }
 
+
+# tidy_element <- function(element, depth = 0){
+#   
+#   out <- element %>% get_attributes # to adapt
+#   
+#   depth_index <- 0
+#   
+#   while(depth_index != depth){
+#     
+#     depth_index <- depth_index + 1
+#     message("Current depth: ", depth_index)
+#     child_col <- paste0("children_", depth_index)
+#     child_id_col <- paste0("id_children_", depth_index)
+#     element_col <- paste0("element_", "depth_index")
+#     
+#     if(depth_index != 1){
+#       parent_col <- dplyr::sym(paste0("children_", depth_index - 1))
+#       if(all(out %>% dplyr::pull({{parent_col}}) %>% purrr::map_lgl(~length(.x) == 0))){
+#         message("Max depth was reached")
+#         break
+#       }
+#     }
+#     
+#     if(depth_index == 1){
+#       # To adapt
+#       out <- out %>% 
+#         dplyr::mutate({{child_col}} := purrr::map2(element, id_parent,~{
+#           .x %>%
+#             xml2::xml_children() %>%
+#             purrr::map_dfr(get_attributes) %>%
+#             dplyr::mutate(id_parent = .y, {{child_id_col}} := 1:n(),
+#                    depth = depth_index)
+#         }))
+#     } else {
+#       
+#       # To adapt
+#       out <- out %>%
+#         dplyr::mutate(parents = {{parent_col}}) %>%
+#         dplyr::mutate({{child_col}} := purrr::map(parents, ~{
+#           if(length(.x) != 0){
+#             parent <- .x
+#             
+#             parent %>%
+#               split(1:nrow(.)) %>%
+#               purrr::imap_dfr(~{
+#                 children <- purrr::reduce(.x$element, c) %>%
+#                   rvest::html_children()
+#                 
+#                 if(length(children) > 0){
+#                   children_attr <- children %>%
+#                     purrr::map_dfr(get_attributes) %>%
+#                     dplyr::filter(!is.null(element)) %>%
+#                     dplyr::select(-id_parent) %>%
+#                     cbind(dplyr::select(.x, dplyr::contains("id_"))) %>%
+#                     dplyr::as_tibble() %>%
+#                     dplyr::mutate({{child_id_col}} := 1:dplyr::n(),
+#                            depth = depth_index)
+#                 }
+#               })
+#           }
+#         })) %>%
+#         dplyr::select(-parents)
+#     }
+#   }
+#   out <-   out %>%
+#     dplyr::select(dplyr::contains("children")) %>%
+#     purrr::reduce(dplyr::bind_rows) %>%
+#     dplyr::bind_rows(dplyr::select(out, -dplyr::contains("children_"))) %>%
+#     # select(id, class, id_parent, contains("id_children"), everything()) %>%
+#     # glimpse
+#     dplyr::mutate_at(dplyr::vars(dplyr::contains("id_children"), id_parent), as.character) %>%
+#     split(1:nrow(.)) %>%
+#     purrr::map_dfr(get_unique_id) %>%
+#     dplyr::mutate(text = purrr::map_chr(element, ~{
+#       .x %>%
+#         rvest::html_text() %>%
+#         stringr::str_squish()
+#     })) %>%
+#     dplyr::select(.id, element, text, dplyr::everything())
+#   
+#   return(out)
+# }
+
+# 
+# 
+# tidy_element_sel <- function(elems, depth = 0){
+#   
+#   out <- elems %>%
+#     purrr::map_dfr(get_all_attribute) %>%
+#     dplyr::mutate(id_parent = 1:n())
+#   
+#   depth_index <- 0
+#   
+#   while(depth_index != depth){
+#     
+#     depth_index <- depth_index + 1
+#     message("Current depth: ", depth_index)
+#     child_col <- paste0("children_", depth_index)
+#     child_id_col <- paste0("id_children_", depth_index)
+#     element_col <- paste0("element_", "depth_index")
+#     
+#     if(depth_index != 1){
+#       parent_col <- dplyr::sym(paste0("children_", depth_index - 1))
+#       if(all(out %>% dplyr::pull({{parent_col}}) %>% purrr::map_lgl(~length(.x) == 0))){
+#         message("Max depth was reached")
+#         # out <- out %>% select(-parent_col)
+#         return(out)
+#       }
+#     }
+#     
+#     if(depth_index == 1){
+#       
+#       out <- out %>%
+#         dplyr::mutate({{child_col}} := purrr::map2(element, id_parent,~{
+#           .x %>%
+#             find_children("*", "xpath") %>%
+#             purrr::map_dfr(get_all_attribute) %>%
+#             dplyr::mutate(id_parent = .y,
+#                    {{child_id_col}} := 1:dplyr::n(),
+#                    depth = depth_index)
+#         }))
+#     } else {
+#       
+#       out <- out %>%
+#         dplyr::mutate(parents = {{parent_col}}) %>%
+#         dplyr::mutate({{child_col}} := purrr::map(parents, ~{
+#           if(length(.x) != 0){
+#             parent <- .x
+#             # child <- children[[1]]
+#             
+#             parent %>%
+#               split(1:nrow(.)) %>%
+#               purrr::imap_dfr(~{
+#                 children <- .x$element[[1]] %>%
+#                   find_children("*", "xpath")
+#                 
+#                 if(length(children) > 0){
+#                   children_attr <- children %>%
+#                     purrr::map_dfr(get_all_attribute) %>%
+#                     cbind(dplyr::select(.x, dplyr::contains("id_"))) %>%
+#                     dplyr::as_tibble() %>%
+#                     dplyr::mutate({{child_id_col}} := 1:dplyr::n(),
+#                            depth = depth_index)
+#                 }
+#               })
+#           }
+#         })) %>%
+#         dplyr::select(-parents) 
+#     }
+#   }
+#   
+#   out <-   out %>%
+#     dplyr::select(dplyr::contains("children")) %>%
+#     purrr::reduce(dplyr::bind_rows) %>%
+#     dplyr::bind_rows(dplyr::select(out, -dplyr::contains("children_"))) %>%
+#     # select(id, class, id_parent, contains("id_children"), everything()) %>%
+#     # glimpse
+#     dplyr::mutate_at(dplyr::vars(dplyr::contains("id_children"), id_parent), as.character) %>%
+#     split(1:nrow(.)) %>%
+#     purrr::map_dfr(get_unique_id) %>%
+#     dplyr::mutate(text = purrr::map_chr(element, ~{
+#       .x %>%
+#         tidyselenium::get_text() %>%
+#         stringr::str_squish()
+#     })) %>%
+#     dplyr::select(.id, element, text, dplyr::everything())
+#   
+#   return(out)
+#   
+# }
+# 
--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,4 @@
 #' quiet
-#' @export
 quiet <- function(x) {
   sink(tempfile())
   on.exit(sink())
@@ -7,7 +6,6 @@ quiet <- function(x) {
 }
 
 #' silently
-#' @export
 silently <- function(x){
   suppressMessages(suppressWarnings(x))
 }
@@ -17,4 +15,61 @@ push_github <- function(message = ""){
   system(glue::glue("git add -A && git commit -m {message} && git push"))
 }
 
+#' innerHTML
+innerHTML <- function(x, trim = FALSE, collapse = "\n"){
+  paste(xml2::xml_find_all(x, ".//text()"), collapse = collapse)
+}
 
+#' outerHTML
+outerHTML <- function(x, trim = FALSE, collapse = "\n"){
+  paste(xml2::xml_find_all(x, "*"), collapse = collapse)
+}
+
+#' get_attributes
+#' @export
+get_attributes <- function(element, text = F){
+  tmp <- element %>%
+    as.character %>%
+    stringr::str_extract("<.*?>") %>%
+    stringr::str_extract_all('\\w+=\\".*?\\"') %>% .[[1]] %>%
+    stringr::str_split("\\=", n = 2) %>%
+    purrr::map_dfc(~{
+      tibble::tibble(stringr::str_remove_all(.x[2], '"')) %>%
+        purrr::set_names(.x[1]) 
+    }) %>%
+    dplyr::mutate(element = list(element))
+  
+  if(nrow(tmp) == 0){tmp <- tibble::tibble(element = list(element))}
+  
+  out <- tmp 
+  
+  if(text){
+    out <- out %>% dplyr::mutate(text = element %>% purrr::map_chr(rvest::html_text))
+  }
+  return(out)
+}
+
+#' get_unique_id
+#' @export
+get_unique_id <- function(d){
+  max <- d$depth
+  
+  if(is.na(max)){
+    d %>%
+      dplyr::mutate(.id = id_parent) %>%
+      dplyr::select(-dplyr::contains("id_"))
+  } else {
+    
+    id_col <- paste0("id_children_", max)
+    to_use <- paste0("id_children_", 1:max)
+    
+    id <- d[,to_use] %>%
+      t %>%
+      paste0(collapse = "_") %>%
+      paste(d$id_parent, .,  sep = "_")
+    
+    d %>%
+      select(-contains("id_"), -depth) %>%
+      mutate(.id = {{id}})
+  }
+}
